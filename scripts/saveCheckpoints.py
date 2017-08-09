e16e1c94830090f0ce6a87b6c919df811a954d8a
Lea <berthomier.lea@gmail.com>
2017-08-09 12:26:28 +0200

    modif saveCheckpoints
    Change tree file and parse games

--- scripts/saveCheckpoints.py
+++ scripts/saveCheckpoints.py
@@ -6,45 +6,43 @@ import time
 """Pour arreter la sauvegarde des checkpoints, creer un fichier STOP
 dans le dossier checkpoints_saved"""
 
-def game_dirs(args, game_name):
-    path = args.debugging_folder+"checkpoints_saved"
-    pathDest = path+"/"+game_name
-    pathGameCP = path+"/"+game_name+"/checkpoints"
-    pathSrc = args.debugging_folder+game_name+"/args.json"
-    if not os.path.exists(pathGameCP):
-        os.makedirs(pathGameCP)
-    if os.path.exists(pathSrc):
-        shutil.copy(pathSrc, pathDest)
-
-def init_dirs(args):
+def init_dirs(args, games):
     path = args.debugging_folder+"checkpoints_saved"
     if not os.path.exists(path):
         os.makedirs(path)
-    if('po' in args.games): game_dirs(args, "pong")
-    if('br' in args.games): game_dirs(args, "breakout")
-    if('ms' in args.games): game_dirs(args, "ms_pacman")
-    if('sp' in args.games): game_dirs(args, "space_invaders")
-    if('mo' in args.games): game_dirs(args, "montezuma_revenge")
-    if('se' in args.games): game_dirs(args, "seaquest")
+    for game_name in games :
+        pathGame = args.debugging_folder+"checkpoints_saved/"+game_name
+        if not os.path.exists(pathGame):
+            os.makedirs(pathGame)
 
-def save_checkpoints(args, game_name):
+def save_checkpoints(args, game_name, n):
     if os.path.exists( args.debugging_folder+"/"+game_name):
-        pathSrc = args.debugging_folder+"/"+game_name+"/checkpoints"
-        pathDest = args.debugging_folder+"checkpoints_saved/"+game_name+"/checkpoints"
-        for f in os.listdir(pathSrc):
-            shutil.copy((pathSrc+"/"+f), pathDest)
+        pathSrc = args.debugging_folder+"/"+game_name
+        pathDest = args.debugging_folder+"checkpoints_saved/"+game_name+"/"+str(n)
+        if not os.path.exists(pathDest):
+            os.makedirs(pathDest)
+        shutil.copy((pathSrc+"/args.json"), pathDest)
+        shutil.copy((pathSrc+"/checkpoints"), pathDest)
+
+def parse_games(args):
+    games = []
+    if('po' in args.games): games.append("pong")
+    if('br' in args.games): games.append("breakout")
+    if('ms' in args.games): games.append("ms_pacman")
+    if('sp' in args.games): games.append("space_invaders")
+    if('mo' in args.games): games.append("montezuma_revenge")
+    if('se' in args.games): games.append("seaquest")
+    return games
 
 def main(args):
-    init_dirs(args)
+    games = parse_games(args)
+    init_dirs(args, games)
     pathSTOP = args.debugging_folder+"checkpoints_saved/STOP"
+    n = 0
     while not(os.path.exists(pathSTOP)):
-        init_dirs(args)
-        if('po' in args.games): save_checkpoints(args, "pong")
-        if('br' in args.games): save_checkpoints(args, "breakout")
-        if('ms' in args.games): save_checkpoints(args, "ms_pacman")
-        if('sp' in args.games): save_checkpoints(args, "space_invaders")
-        if('mo' in args.games): save_checkpoints(args, "montezuma_revenge")
-        if('se' in args.games): save_checkpoints(args, "seaquest")
+        init_dirs(args, games)
+        for g in games :
+            save_checkpoints(args, g)
         time.sleep(args.period)
 
 def get_arg_parser():
