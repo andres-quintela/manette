feb557e6086acbef5c4fbf28643c7e98057ce0b4
Lea <berthomier.lea@gmail.com>
2017-08-25 16:19:31 +0200

    WIP rewriting genGifs.py

--- scripts/genGifs.py
+++ scripts/genGifs.py
@@ -3,11 +3,11 @@ import subprocess
 import os
 
 
-def create_cmd(args, game_name):
-    cmd = ("python3 test.py -f "+args.folder+game_name+"/"+
+def create_cmd_all_final(args, f):
+    cmd = ("python3 test.py -f "+args.folder+f+"/"+
            " -tc "+str(args.test_count)+
            " -np "+str(args.noops)+
-           " -gn "+game_name+
+           " -gn "+f+
            " -gf "+args.folder+"gifs/"
            " -d "+args.device)
     return cmd
@@ -16,24 +16,21 @@ def main(args):
     path = args.folder+"gifs"
     if not os.path.exists(path):
         os.makedirs(path)
-    if('po' in args.games):
-        subprocess.call(create_cmd(args, "pong"), shell = True)
-    if('br' in args.games):
-        subprocess.call(create_cmd(args, "breakout"), shell = True)
-    if('ms' in args.games):
-        subprocess.call(create_cmd(args, "ms_pacman"), shell = True)
-    if('sp' in args.games):
-        subprocess.call(create_cmd(args, "space_invaders"), shell = True)
-    if('mo' in args.games):
-        subprocess.call(create_cmd(args, "montezuma_revenge"), shell = True)
-    if('se' in args.games):
-        subprocess.call(create_cmd(args, "seaquest"), shell = True)
+    if args.game_folder == '' :
+        for f in os.listdir(args.folder):
+            if args.checkpoint == 0 :
+                subprocess.call(create_cmd_all_final(args, f), shell = True)
+            
+
+
+
 
 def get_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', default= 'po', type=str, help='Name of the games to train', dest='games')
+    parser.add_argument('-gf', default= '', type=str, help='Name of the games folder to generate. Default : all folder are generated.', dest='game_folder')
     parser.add_argument('-f', '--folder', type=str, help="Folder where is saved the logs of all games.", dest="folder", required=True)
     parser.add_argument('-tc', '--test_count', default='1', type=int, help="The amount of tests to run on the given network", dest="test_count")
+    parser.add_argument('-cp', '--checkpoint', default='0', type=int, help="The checkpoint from which to run the test", dest="checkpoint")
     parser.add_argument('-np', '--noops', default=30, type=int, help="Maximum amount of no-ops to use", dest="noops")
     parser.add_argument('-d', '--device', default='/gpu:0', type=str, help="Device to be used ('/cpu:0', '/gpu:0', '/gpu:1',...)", dest="device")
     return parser
