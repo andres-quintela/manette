b078eae7476dbf43ea8a14194ba80f908ddc9663
Lea <berthomier.lea@gmail.com>
2017-09-08 12:14:58 +0200

    debug makeVideos

--- scripts/makeVideos.py
+++ scripts/makeVideos.py
@@ -3,16 +3,16 @@ import os
 import argparse
 
 
-def create_cmd_convert(n, path, game):
-    gif_name = path+str(n)+'0.gif'
-    video_name = path+str(n)+".mp4"
+def create_cmd_convert(n, pathSrc, pathDest):
+    gif_name = pathSrc+str(n)+'0.gif'
+    video_name = pathDest+str(n)+".mp4"
     cmd = ('ffmpeg -i '+gif_name+' -movflags faststart -pix_fmt yuv420p'+
            ' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" '+video_name)
     return cmd
 
 def main(args):
-    pathSrc = args.folder+"trainingGifs/"
-    pathDest = args.folder+"trainingVideos/"
+    pathSrc = args.folder+"training_gifs/"
+    pathDest = args.folder+"training_videos/"
     if not os.path.exists(pathDest):
         os.makedirs(pathDest)
     f_list = os.listdir(pathSrc)
@@ -21,7 +21,7 @@ def main(args):
         name_list.append(int(f[:-5]))
     for n in name_list:
         print(str(n)+" / "+str(len(name_list)))
-        subprocess.call(create_cmd_convert(n, pathSrc), shell = True)
+        subprocess.call(create_cmd_convert(n, pathSrc, pathDest), shell = True)
 
 def get_arg_parser():
     parser = argparse.ArgumentParser()
