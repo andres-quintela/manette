2b64e7c851badf52ba994b2f80ce0e1c9ed2375f
Lea <berthomier.lea@gmail.com>
2017-09-18 14:47:17 +0200

    debug histogram actions

--- paac.py
+++ paac.py
@@ -240,8 +240,11 @@ class PAACLearner(ActorLearner):
             for i in range(self.total_repetitions) :
                 histo_r += [i]*int(nb_r[i])
 
-            tf.summary.histogram("actions", histo_a)
-            tf.summary.histogram("repetitions", histo_r)
+            histo_a_var = tf.Variable(histo_a)
+            histo_r_var = tf.Variable(histo_r)
+
+            tf.summary.histogram("actions", histo_a_var)
+            tf.summary.histogram("repetitions", histo_r_var)
 
             self.summary_writer.flush()
 
