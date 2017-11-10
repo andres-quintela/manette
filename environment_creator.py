31afcacbb7896f4bfdf435e5fbc5d712a8c8f98c
Lea <berthomier.lea@gmail.com>
2017-11-10 17:03:19 +0100

    add tetris

--- environment_creator.py
+++ environment_creator.py
@@ -3,11 +3,15 @@ class EnvironmentCreator(object):
     def __init__(self, args):
         """ Creates an object from which new environments can be created.
             Param : args. """
-
-        from atari_emulator import AtariEmulator
-        from ale_python_interface import ALEInterface
-        filename = args.rom_path + "/" + args.game + ".bin"
-        ale_int = ALEInterface()
-        ale_int.loadROM(str.encode(filename))
-        self.num_actions = len(ale_int.getMinimalActionSet())
-        self.create_environment = lambda i: AtariEmulator(i, args)
+        if args.game == 'tetris' :
+            from tetris_emulator import TetrisEmulator
+            self.num_actions = 5
+            self.create_environment = lambda i: TetrisEmulator(i, args)
+        else :
+            from atari_emulator import AtariEmulator
+            from ale_python_interface import ALEInterface
+            filename = args.rom_path + "/" + args.game + ".bin"
+            ale_int = ALEInterface()
+            ale_int.loadROM(str.encode(filename))
+            self.num_actions = len(ale_int.getMinimalActionSet())
+            self.create_environment = lambda i: AtariEmulator(i, args)
